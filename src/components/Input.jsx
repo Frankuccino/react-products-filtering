f02c1d61a044b692296572ee90c5eb68af91e2f1
Frank Angelo Malubag <frankabad189@gmail.com>
2026-05-04 18:52:14 +0800

    feat: Created Reusable Component - input radio with Props of group and name

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,11 @@
-const Input = () => {
-  return <div>Input</div>;
+const Input = ({ group, name }) => {
+  return (
+    <label className="sidebar-label-container">
+      <input type="radio" name={group} />
+      <span className="checkmark"></span>
+      {name}
+    </label>
+  );
 };
 
 export default Input;

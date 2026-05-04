24f3486d7cc100cfc1a3ab62ff10b71b29f9fdd1
Frank Angelo Malubag <frankabad189@gmail.com>
2026-05-04 18:40:26 +0800

    refactor: Added the Props and the proper JSX fields for the props.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,18 @@
-import { AiFillStar } from "react-icons/ai";
 import { BsBagHeartFill } from "react-icons/bs";
 
-const Card = () => {
+const Card = ({ img, title, star, reviews, prevPrice, newPrice }) => {
   return (
     <figure className="card">
-      <img
-        src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
-        alt="Shoe"
-        className="card-img"
-      />
+      <img src={img} alt={title} className="card-img" />
       <figcaption className="card-details">
-        <h3 className="card-title">Shoe</h3>
+        <h3 className="card-title">{title}</h3>
         <section className="card-reviews">
-          <div className="stars">
-            <AiFillStar className="ratings-start" />
-            <AiFillStar className="ratings-start" />
-            <AiFillStar className="ratings-start" />
-            <AiFillStar className="ratings-start" />
-            <AiFillStar className="ratings-start" />
-          </div>
-          <span className="total-reviews">4 Stars</span>
+          <div className="stars">{star}</div>
+          <span className="total-reviews">{reviews}</span>
         </section>
         <section className="card-price">
           <div className="price">
-            <del>#300</del> 200
+            <del>{prevPrice}</del> {newPrice}
           </div>
           <div className="bag">
             <BsBagHeartFill />

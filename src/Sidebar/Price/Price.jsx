560e651983d873036d42dac164b3e03ff0930703
Frank Angelo Malubag <frankabad189@gmail.com>
2026-05-04 19:00:02 +0800

    refactor: Added the priceRanges Array to be mapped over and return an Input Component passing the range values as the name property and group as prices

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -1,29 +1,21 @@
+import Input from "../../components/Input";
 import "./Price.css";
 
 const Price = () => {
+  const priceRanges = [
+    "All",
+    "$0 - $50",
+    "$50 - $100",
+    "$100 - $150",
+    "Over $150",
+  ];
+
   return (
     <div className="ml">
       <h2 className="sidebar-title price-title">Price</h2>
-      <label className="sidebar-label-container">
-        <input type="radio" name="test2" />
-        <span className="checkmark"></span> All
-      </label>
-      <label className="sidebar-label-container">
-        <input type="radio" name="test2" />
-        <span className="checkmark"></span> $0 - $50
-      </label>
-      <label className="sidebar-label-container">
-        <input type="radio" name="test2" />
-        <span className="checkmark"></span> $50 - $100
-      </label>
-      <label className="sidebar-label-container">
-        <input type="radio" name="test2" />
-        <span className="checkmark"></span> $100 - $150
-      </label>
-      <label className="sidebar-label-container">
-        <input type="radio" name="test2" />
-        <span className="checkmark"></span> Over $150
-      </label>
+      {priceRanges.map((range) => (
+        <Input group="prices" name={range} />
+      ))}
     </div>
   );
 };
